0ac948c8145334158a7523de162ef0c81cd57fdc
PETER MUYOMBA <111797748+pmuyomba11@users.noreply.github.com>
2023-11-03 14:39:30 -0400

    create user model

---
+++ api/models/user.model.js
@@ -0,0 +1,20 @@
+import mongoose, { Schema } from "mongoose";
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true
+    }
+}, { timestamps: true })
+
+const User = mongoose.model('User', userSchema)
+
+export default User;
